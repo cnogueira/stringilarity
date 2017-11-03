c7dd8efd8dddc712e6ed5117eb6d3d1ce5817b83
Cristofor Nogueira <cristofor.nogueira@gmail.com>
2017-11-03 10:34:22 +0100

    dice coefficient does not dicscard repeated ngrams

--- lib/stringilarity.ex
+++ lib/stringilarity.ex
@@ -9,25 +9,37 @@ defmodule Stringilarity do
     0.25
 
   """
-  def dice(a, b) do
-    bigrams_a = a |> to_bigram_set
-    bigrams_b = b |> to_bigram_set
-    common_bigrams = MapSet.intersection(bigrams_a, bigrams_b)
+  def dice(a, b, ngram_size \\ 2) do
+    ngrams_a = a |> to_ngram_frequency_map(ngram_size)
+    ngrams_b = b |> to_ngram_frequency_map(ngram_size)
+    ngrams_common_count = count_intersect_freq(ngrams_a, ngrams_b)
 
-    2*MapSet.size(common_bigrams) / (MapSet.size(bigrams_a) + MapSet.size(bigrams_b))
+    2*ngrams_common_count / (count_freq(ngrams_a) + count_freq(ngrams_b))
   end
 
-  defp to_bigram_set(word) do
+  defp to_ngram_frequency_map(word, ngram_size) do
     word
+    |> String.downcase
     |> String.codepoints
-    |> n_grams(2)
-    |> MapSet.new
+    |> n_grams_frequency_map(ngram_size)
   end
 
-  defp n_grams(list, n) when length(list) < n, do: []
-  defp n_grams([_head | tail] = list, n) do
-    ngram = list |> Enum.take(n) |> List.to_string
+  defp n_grams_frequency_map(list, ngram_size) when length(list) < ngram_size, do: %{}
+  defp n_grams_frequency_map([_head | tail] = list, ngram_size) do
+    ngram = list |> Enum.take(ngram_size) |> List.to_string
 
-    [ngram | n_grams(tail, n)]
+    n_grams_frequency_map(tail, ngram_size)
+    |> Map.update(ngram, 1, & &1+1)
+  end
+
+  # Computes the intersection of two frequency maps, counting for each element
+  # the amount it appears in both sets
+  defp count_intersect_freq(a, b) when map_size(a) > map_size(b), do: count_intersect_freq(b, a)
+  defp count_intersect_freq(freq_a, freq_b) do
+    Enum.reduce(freq_a, 0, fn {key, freq}, acc -> acc + min(freq, Map.get(freq_b, key, 0)) end)
+  end
+
+  defp count_freq(freq_map) do
+    Enum.reduce(freq_map, 0, fn {_, freq}, acc -> acc + freq end)
   end
 end
--- test/stringilarity_test.exs
+++ test/stringilarity_test.exs
@@ -3,6 +3,16 @@ defmodule StringilarityTest do
   doctest Stringilarity
 
   test "sorensen-dice similarity coefficient" do
-    assert Stringilarity.dice("night", "nocht") == 0.25
+    assert Stringilarity.dice("night", "nocht", 1) == 0.6
+    assert Stringilarity.dice("context", "contact", 1) == 0.7142857142857143
+    assert Stringilarity.dice("go", "gogogogo") == 0.25
+  end
+
+  test "sorensen-dice similarity coefficient works with digrams by default" do
+    assert Stringilarity.dice("whatever", "whomever") == Stringilarity.dice("whatever", "whomever", 2)
+  end
+
+  test "sorensen-dice similrity coefficient is case insensitive" do
+    assert Stringilarity.dice("night", "nocht") == Stringilarity.dice("NIGHT", "nocht")
   end
 end
